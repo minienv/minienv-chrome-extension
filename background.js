876c7d43539a79a2bd0f5a5ce0bd405550adc64d
Mark Watson <mark.watson.x@gmail.com>
2017-06-16 01:51:08 -0500

    Initial commit

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.runtime.onInstalled.addListener(function() {
+   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+         conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+               css: ["a[href$='docker-compose.yml']"],
+               pageUrl: {
+                  hostEquals: 'github.com'
+               }
+            })
+         ],
+         actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+   });
+});
+chrome.pageAction.onClicked.addListener(function(tab){
+  var url = "http://exampleup.com:32155?repo=" + encodeURIComponent(tab.url);
+  chrome.tabs.create({url: url});
+});
